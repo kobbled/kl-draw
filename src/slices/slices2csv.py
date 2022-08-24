b41131aa0e6bfcc64b0da2636060a231f6892150
halmusaibeli <80288863+halmusaibeli@users.noreply.github.com>
2022-08-24 10:52:14 -0600

    add the ability to downsample contours

--- src/slices/slices2csv.py
+++ src/slices/slices2csv.py
@@ -43,65 +43,60 @@ def gcode_get_params(filename):
 
 # convert slices (binary images) to 2D contours
 def img2contour(Xresolution, Yresolution, Xdimension, Ydimension, totalLayers, zlayers, foldername):
-    fig = plt.figure()
-    ax = plt.axes(projection='3d')
-    xdata = []; ydata = []; zdata = []
-    points =[]
+    model_points = []
     polygons = []
     for layer in tqdm(range(1,totalLayers+1)):
         # reading layer image
         img = cv2.imread(foldername + '\\' + str(layer) + '.png', cv2.IMREAD_GRAYSCALE)
         ret, binaryImg = cv2.threshold(img, 100, 255, cv2.THRESH_OTSU)
-        contours, hierarchy = cv2.findContours(binaryImg, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contours, hierarchy = cv2.findContours(binaryImg, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
-        """
-        # To show contours      -----------------------
-        # convert to rgb for drawing contours later
-        rgbImg = cv2.cvtColor(img,cv2.COLOR_GRAY2RGB)
-        cv2.imshow('Binary image', binaryImg)
-        cv2.waitKey(0) # Wait for keypress to continue        
-        with_contours = cv2.drawContours(rgbImg, contours, -1, (255,0,0),3)
-        cv2.imshow('Detected contours', with_contours)
-        cv2.waitKey(0)
-        """
-
-        """        
-        # contour leveling   hierarchy [Next, Previous, Child, Parent]
-        # find the most external contour, which is the starting contour going inside
-        outerContoursIndex = []
-        for h in hierarchy:
-            if h[2] < 0:   # does not have parent
-                outerContoursIndex.append(hierarchy.index(h))
-
-        levels = np.array(len(hierarchy))
-        contourIndex = outerContoursIndex   # starting from the outer contour
-        for i in len(hierarchy):
-            levels[contourIndex] = i
-            contourIndex = hierarchy.indx(hierarchy[contourIndex][2])  # searching for the index of the child
-        print(levels)
+        """"
+        # To show contours
+        if layer == args.show_layer:
+            # convert to rgb for drawing contours later
+            rgbImg = cv2.cvtColor(img,cv2.COLOR_GRAY2RGB)    
+            with_contours = cv2.drawContours(rgbImg, contours, -1, (255,0,0),3)
+            cv2.imshow('Detected contours', with_contours)
+            cv2.waitKey(0)
         """
         count_polygons = 1
         # interpret contours in layer
-        for contour, level in zip(contours,hierarchy[0]) :
+        for contour, level in zip(contours,hierarchy[0]):
+            points = []
             for p in contour:
-                # converting from pixcels to mm (mm/pxl)
+                # converting from pixels to mm (mm/pxl)
                 mapped_point = [p[0][0] * (Xdimension / Xresolution), p[0][1] * (Ydimension / Yresolution), zlayers[layer-1]]  
                 points.append(mapped_point)
-                polygons.append(["%.2f" % mapped_point[0], "%.2f" % mapped_point[1], "%.2f" % mapped_point[2], "%d" % layer, "%d" % count_polygons, "%d" % (level[3]+2) ])
-                xdata.append(mapped_point[0]); ydata.append(mapped_point[1]); zdata.append(mapped_point[2])
-            count_polygons = count_polygons + 1
-    pcd = o3d.geometry.PointCloud(o3d.utility.Vector3dVector(np.array(points)))
-    downsampled = pcd.voxel_down_sample(voxel_size=0.0001)
-    # visualize contours
-    o3d.visualization.draw_geometries([downsampled])
-    return polygons
+            
+            # voxel downsampling using open3d
+            if args.sampling_magnitude != 0:
+                points_obj = (o3d.geometry.PointCloud(o3d.utility.Vector3dVector(np.array(points))))       # convert to open3d format
+                sampled_points = np.asarray(points_obj.voxel_down_sample(voxel_size= args.sampling_magnitude).points) # sampled points array
+            else:
+                sampled_points = points
+            
+            # store points in polygon to export them in csv
+            for sp in sampled_points:
+                polygons.append(["%.2f" % sp[0], "%.2f" % sp[1], "%.2f" % sp[2], "%d" % layer, "%d" % count_polygons, "%d" % (level[3]+2) ])
+            
+            # store all points for
+            model_points.extend(sampled_points)
 
+            # next contour
+            count_polygons = count_polygons + 1
+    
+    if args.vis:
+        # visualize contours using Open3D
+        o3d.visualization.draw_geometries([o3d.geometry.PointCloud(o3d.utility.Vector3dVector(np.array(model_points)))])
+    
+    return polygons
 
 def main(args):
     # model slices file
     zipdir = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', '..')) + '\\test\\stl_examples\\'
     csvdir = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', '..')) + '\\test\\csv_examples\\'
-    zipfile = zipdir + args.zip_file
+    zipfile = zipdir + args.zip_file 
     csvfile = csvdir + args.csv_file
     
     # extract file in zip file 
@@ -123,11 +118,18 @@ def main(args):
 
 
 if __name__ == "__main__":
-    description=("Converts Images slices of a model to contours (CSV), assumming zip file in stl_examples, and saving csv file to csv_examples.")
+    description=("""Converts Images slices of a model to contours (CSV), assumming zip file in stl_examples, and saving csv file to csv_examples.)
+    
+    Examples:  
+
+    python slices2csv.py _rocket_nozzle.zip _rocket_nozzle.csv -sm 5 -vis                 # sampling size is 5 
+     python slices2csv.py _rocket_nozzle.zip _rocket_nozzle.csv -vis          # don't sample points, but show contours""")
 
     parser = argparse.ArgumentParser(prog='slices2csv', description=description, formatter_class=argparse.MetavarTypeHelpFormatter)
     parser.add_argument('zip_file', type=str, nargs='?', metavar='ZIP', help="zip file")
     parser.add_argument('csv_file', type=str, nargs='?', metavar='CSV', help="csv file")
+    parser.add_argument('-sm', '--sampling_magnitude', default=0, type=int, help='contour sampling magnitude.', nargs='?')
+    parser.add_argument('-vis', action='store_true', help='visualization contours flag')
     args = parser.parse_args()
 
     main(args)
